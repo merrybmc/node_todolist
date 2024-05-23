8160437eaae1e70c16bd09794a5b0271ffc17906
Byeong Min Cho <merrybmc@gmail.com>
2024-05-24 01:05:23 +0900

    put, delete mongoose method로 변경

--- todo-be/entitys/tasks/service/task.service.js
+++ todo-be/entitys/tasks/service/task.service.js
@@ -38,7 +38,18 @@ taskService.putTask = async (req, res, next) => {
 
     const { id } = req.params;
     const { isComplete } = req.body;
-    const putTask = await Task.updateOne({ _id: id }, { $set: { isComplete } });
+
+    // case 1 mongodb
+    // const putTask = await Task.updateOne({ _id: id }, { $set: { isComplete } });
+
+    // case 2 $set 생략 가능
+    // const putTask = await Task.updateOne({ _id: id }, { isComplete });
+
+    // case 2 mongoose findByIdUpdate
+    // parameter 1 = id 값 전달, 알아서 추적해냄
+    // parameter 2 = 바꿀 객체 전달
+    // parameter 3 = 옵션 객체 {new: true} = 변경사항이 반영된 값 전달 여부
+    const putTask = await Task.findByIdAndUpdate(id, { isComplete }, { new: true });
 
     req.statusCode = 200;
     req.data = putTask;
@@ -55,12 +66,15 @@ taskService.deleteTask = async (req, res, next) => {
 
     const { id } = req.params;
 
-    const deleteTask = await Task.deleteOne({ _id: id });
+    // const deleteTask = await Task.deleteOne({ _id: id });
+    // findByIdAndDelete는 id값만 전달, 옵션 객체는 일반적으로 많이 쓰이지 않음
+    const deleteTask = await Task.findByIdAndDelete(id);
 
     req.statusCode = 200;
     req.data = deleteTask;
   } catch (e) {
     req.statusCode = 400;
+    ㅏ;
     req.error = e.message;
   }
   next();
